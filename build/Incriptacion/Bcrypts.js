8cd06be52f00d7d6c29d689570fe1db44eb611af
victorguti <victor.guiterrez@correounivalle.edu.co>
2020-08-21 21:29:15 -0500

    Cdn

--- build/Incriptacion/Bcrypts.js
+++ build/Incriptacion/Bcrypts.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.encriptacion = void 0;
 class Encriptacion {
     constructor() {
         this.bcrypt = require('bcryptjs');
